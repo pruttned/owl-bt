9b7f2058357bd34eab909836d78d3d01f501606f
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-05-02 20:11:49 +0200

    feat(client/treeModelConverter): add conversion from runtime to store

--- editor/client/components/treeModelConverter/treeModelConverter.service.js
+++ editor/client/components/treeModelConverter/treeModelConverter.service.js
@@ -11,14 +11,54 @@
       this.nextId = 1;
     }
 
-    newId() {
+    convertNodeToRuntime(nodeData) {
+      let node = {};
+      angular.extend(node, nodeData);
+      this._addNodeMetaInfo(node);
+      node.properties = this._convertPropertiesToRuntime(node.properties);
+
+      node.decorators = node.decorators ? node.decorators.map(d => this._convertSubItemToRuntime(d)) : [];
+      node.services = node.services ? node.services.map(s => this._convertSubItemToRuntime(s)) : [];
+      node.childNodes = node.childNodes ? node.childNodes.map(n => this.convertNodeToRuntime(n)) : [];
+
+      return node;
+    }
+
+    convertNodeToStore(node) {
+      let data = {};
+      angular.extend(data, node);
+
+      delete data._meta;
+
+      if (data.properties) {
+        data.properties = this._convertPropertiesToStore(data.properties);
+        if(_.isEmpty(data.properties)){
+          delete data.properties;
+        }
+      }
+      if (data.decorators) {
+        data.decorators = data.decorators.map(d => this._convertSubItemToStore(d));
+      }
+      if (data.services) {
+        data.services = data.services.map(s => this._convertSubItemToStore(s));
+      }
+      if (data.childNodes) {
+        data.childNodes = data.childNodes.map(n => this.convertNodeToStore(n));
+      }
+
+      this._clearEmptyArrays(data);
+
+      return data;
+    }
+
+    _newId() {
       if (this.nextId === Number.MAX_SAFE_INTEGER) {
         throw new Error('max id reached');
       }
       return this.nextId++;
     }
 
-    convertPropertiesToRuntime(propsArray) {
+    _convertPropertiesToRuntime(propsArray) {
       let propsObj = {};
       if (propsArray) {
         for (let prop of propsArray) {
@@ -28,34 +68,59 @@
       return propsObj;
     }
 
-    convertSubItemToRuntime(data) {
+    _convertSubItemToRuntime(data) {
       let item = {};
       angular.extend(item, data);
-      item.properties = this.convertPropertiesToRuntime(item.properties);
+      item.properties = this._convertPropertiesToRuntime(item.properties);
       return item;
     }
 
-    convertNodeToRuntime(nodeData) {
-      let node = {};
-      angular.extend(node, nodeData);
-      this.addNodeMetaInfo(node);
-      node.properties = this.convertPropertiesToRuntime(node.properties);
-
-      node.decorators = node.decorators ? node.decorators.map(d => this.convertSubItemToRuntime(d)) : [];
-      node.services = node.services ? node.services.map(s => this.convertSubItemToRuntime(s)) : [];
-      node.childNodes = node.childNodes ? node.childNodes.map(n => this.convertNodeToRuntime(n)) : [];
-
-      return node;
-    }
-
-    addNodeMetaInfo(node) {
+    _addNodeMetaInfo(node) {
       node._meta = {
-        id: this.newId(),
+        id: this._newId(),
         version: 1
       };
     }
 
-    //TODO: runtime to store conversion
+    _convertPropertiesToStore(propsObj) {
+      let propsArray = [];
+      if (propsObj) {
+        for (let prop in propsObj) {
+          if (propsObj.hasOwnProperty(prop)) {
+            propsArray.push({
+              name: prop,
+              value: propsObj[prop]
+            });
+          }
+        }
+      }
+      return propsArray;
+    }
+
+    _convertSubItemToStore(item) {
+      let data = {};
+      angular.extend(data, item);
+      this._clearEmptyArrays(data);
+      if (data.properties) {
+        data.properties = this._convertPropertiesToStore(data.properties);
+        if(_.isEmpty(data.properties)){
+          delete data.properties;
+        }
+      }
+      return data;
+    }
+
+    _clearEmptyArrays(obj) {
+      for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          let value = obj[prop];
+          if (value && _.isArray(value) && !value.length) {
+            delete obj[prop];
+          }
+        }
+      }
+    }
+
   }
 
   angular.module('editorApp')
--- editor/client/components/treeModelConverter/treeModelConverter.service.spec.js
+++ editor/client/components/treeModelConverter/treeModelConverter.service.spec.js
@@ -12,7 +12,7 @@ describe('Service: TreeModelConverter', function() {
   }));
 
   it('convertPropertiesToRuntime should convert property array to object', function() {
-    let res = TreeModelConverter.convertPropertiesToRuntime([{
+    let res = TreeModelConverter._convertPropertiesToRuntime([{
       name: 'prop1',
       value: 'value1'
     }, {
@@ -36,7 +36,7 @@ describe('Service: TreeModelConverter', function() {
         value: 'value2'
       }]
     };
-    let res = TreeModelConverter.convertSubItemToRuntime(item);
+    let res = TreeModelConverter._convertSubItemToRuntime(item);
     expect(res).not.toBe(item);
     expect(res).toEqual({
       type: 'type',
@@ -124,4 +124,83 @@ describe('Service: TreeModelConverter', function() {
       }]
     });
   });
+
+  it('convertNodeToStore should clone node and all its subnodes+subitems; convert all property collections and remove meta inf + remove empty arrays/properties', function() {
+    let node = {
+      type: 'sequence',
+      _meta: {
+        id: 1,
+        version: 1
+      },
+      properties: {},
+      decorators: [{
+        type: 'd1',
+        properties: {
+          prop1: 'value1',
+          prop2: 'value2'
+        }
+      }],
+      services: [{
+        type: 's1',
+        properties: {
+          prop1: 'value1',
+          prop2: 'value2'
+        }
+      }],
+      childNodes: [{
+        type: 'chn1',
+        _meta: {
+          id: 2,
+          version: 1
+        },
+        properties: {
+          prop1: 'value1',
+          prop2: 'value2'
+        },
+        decorators: [],
+        services: [],
+        childNodes: []
+      }]
+    };
+    let res = TreeModelConverter.convertNodeToStore(node);
+    expect(res).not.toBe(node);
+    expect(res.decorators[0]).not.toBe(node.decorators[0]);
+    expect(res.services[0]).not.toBe(node.services[0]);
+    expect(res.childNodes[0]).not.toBe(node.childNodes[0]);
+    expect(res).toEqual({
+      type: 'sequence',
+      decorators: [{
+        type: 'd1',
+        properties: [{
+          name: 'prop1',
+          value: 'value1'
+        }, {
+          name: 'prop2',
+          value: 'value2'
+        }]
+      }],
+      services: [{
+        type: 's1',
+        properties: [{
+          name: 'prop1',
+          value: 'value1'
+        }, {
+          name: 'prop2',
+          value: 'value2'
+
+        }]
+      }],
+      childNodes: [{
+        type: 'chn1',
+        properties: [{
+          name: 'prop1',
+          value: 'value1'
+        }, {
+          name: 'prop2',
+          value: 'value2'
+        }]
+      }]
+    });
+  });
+
 });
