49d964dad192bb8910a5184fbb4a2689b9f6cb94
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-05-15 18:49:42 +0200

    refactor(client/tree): rename propertyTypes dir to propertyEditor

--- editor/client/app/tree/propertyEditor/propertyEditorList.directive.js
+++ editor/client/app/tree/propertyEditor/propertyEditorList.directive.js
@@ -1,28 +1,30 @@
 'use strict';
 
 angular.module('editorApp')
-  .directive('propertyEditorList', function($compile) {
+  .directive('propertyEditorList', function($compile, PropertyAccessorProvider) {
 
     return {
       template: '<ul class="property-list"></ul>',
       restrict: 'EA',
       replace: true,
       scope: {
-        properties: '=',
+        selItem: '=',
       },
 
       link: function(scope, element) {
         let childScope;
-        scope.$watch('properties', function() {
+        scope.$watch('selItem', function() {
           if (childScope) { //no leaking watchers
             childScope.$destroy();
             element.empty();
           }
           childScope = scope.$new();
 
-          if (scope.properties) {
-            for (var i = 0; i < scope.properties.length; i++) {
-              let propertyType = scope.properties[i].desc.type;
+          let selItem = scope.selItem;
+          if(selItem){
+            childScope.properties = PropertyAccessorProvider.createPropertyAccessors(selItem);
+            for (let i = 0; i < childScope.properties.length; i++) {
+              let propertyType = childScope.properties[i].desc.type;
               let template = $compile(`<li class="property-editor"><label>{{properties[${i}].name()}}</label><span class="fa fa-undo icon-button" ng-click="properties[${i}].reset()" ng-if="properties[${i}].isSet()"></span><div ${propertyType}-property-editor property="properties[${i}]">Missing editor for <em>${propertyType}</em> property type</div></li>`);
               element.append(template(childScope));
             }
