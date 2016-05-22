76f1faeebf9b6081f223d67c159b1711fa3e1a36
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-05-22 15:38:08 +0200

    feat(client/treeNode): add methods for getting service/decorator index in its node

--- editor/client/app/tree/treeModel/treeNode.service.js
+++ editor/client/app/tree/treeModel/treeNode.service.js
@@ -7,6 +7,10 @@
    */
   class TreeNode {
 
+    constructor(_) {
+      this._ = _;
+    }
+
     updateVersion(node) {
       if (node.$meta.version === Number.MAX_SAFE_INTEGER) {
         node.$meta.version = 1;
@@ -16,24 +20,38 @@
     }
 
     addService(node, service) {
-      if(!node.services){
+      if (!node.services) {
         node.services = [];
       }
       node.services.push(service);
     }
 
     addDecorator(node, decorator) {
-      if(!node.decorators){
+      if (!node.decorators) {
         node.decorators = [];
       }
       node.decorators.push(decorator);
     }
 
     addChildNode(node, childNode) {
-      if(!node.childNodes){
+      if (!node.childNodes) {
         node.childNodes = [];
       }
       node.childNodes.push(childNode);
+    }
+
+    indexOfService(node, service) {
+      if (node.services) {
+        return this._.indexOf(node.services, service);
+      }
+      return -1;
+    }
+
+    indexOfDecorator(node, decorator) {
+      if (node.decorators) {
+        return this._.indexOf(node.decorators, decorator);
+      }
+      return -1;
     }
   }
 
