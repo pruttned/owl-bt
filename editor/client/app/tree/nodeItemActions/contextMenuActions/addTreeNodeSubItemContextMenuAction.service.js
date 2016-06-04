3b46df0a96fb2c5230939df36a21ee45212af0a7
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-06-04 14:15:46 +0200

    feat(client/tree): add node context menu action for adding sub items

---
+++ editor/client/app/tree/nodeItemActions/contextMenuActions/addTreeNodeSubItemContextMenuAction.service.js
@@ -0,0 +1,60 @@
+'use strict';
+
+(function() {
+
+  class AddTreeNodeSubItemContextMenuAction {
+
+    constructor(_, AddTreeNodeSubItemCmd, ListSelectDialog, ProjectStore, TreeServiceItemProvider, TreeDecoratorItemProvider) {
+      this._ = _;
+      this._AddTreeNodeSubItemCmd = AddTreeNodeSubItemCmd;
+      this._ListSelectDialog = ListSelectDialog;
+      this._ProjectStore = ProjectStore;
+      this._TreeServiceItemProvider = TreeServiceItemProvider;
+      this._TreeDecoratorItemProvider = TreeDecoratorItemProvider;
+    }
+
+    create(node, actionDesc) {
+      let _this = this;
+
+      return {
+        title: actionDesc.title,
+        icon: actionDesc.icon,
+        order: actionDesc.order,
+        action: () => {
+          let descs = _this._.values(_this._ProjectStore.getSubItemDescs(actionDesc.subItemType));
+          return _this._ListSelectDialog.open(descs)
+            .result.then(desc => {
+              let provider = actionDesc.subItemType === 'service' ? _this._TreeServiceItemProvider : _this._TreeDecoratorItemProvider;
+              let subItem = provider.create({
+                type: desc.name
+              });
+              return _this._AddTreeNodeSubItemCmd.exec({
+                node: node,
+                subItem: subItem,
+                subItemType: actionDesc.subItemType
+              });
+            });
+        }
+      };
+    }
+  }
+
+  angular.module('editorApp')
+    .service('AddTreeNodeSubItemContextMenuAction', AddTreeNodeSubItemContextMenuAction)
+    .config(function(NodeItemActionCfgProvider) {
+      NodeItemActionCfgProvider.registerNodeContextMenuAction({
+        service: 'AddTreeNodeSubItemContextMenuAction',
+        title: 'Add Service',
+        icon: 'clock-o',
+        order: 10,
+        subItemType: 'service'
+      });
+      NodeItemActionCfgProvider.registerNodeContextMenuAction({
+        service: 'AddTreeNodeSubItemContextMenuAction',
+        title: 'Add Decorator',
+        icon: 'filter',
+        order: 11,
+        subItemType: 'decorator'
+      });
+    });
+})();
