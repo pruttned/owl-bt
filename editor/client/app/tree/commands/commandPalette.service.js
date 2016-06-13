a8463b68cd51998f7041c80ea1ef5496a5e5ebc8
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-06-13 19:38:58 +0200

    feat(client/tree): add command palette with hotkey support

---
+++ editor/client/app/tree/commands/commandPalette.service.js
@@ -0,0 +1,99 @@
+'use strict';
+
+(function() {
+
+  class CommandPalette {
+    constructor(_, $q, $injector, hotkeys, CommandPaletteCfg, ListSelectDialog) {
+      let _this = this;
+
+      this._ = _;
+      this._$q = $q;
+      this._ListSelectDialog = ListSelectDialog;
+
+      this._commands = [];
+      for (let cmdDesc of CommandPaletteCfg.commands) {
+        $injector.invoke([cmdDesc.service, cmdSvc => {
+          this._commands.push({
+            service: cmdSvc,
+            name: cmdDesc.name,
+            icon: cmdDesc.icon,
+            shortcut: cmdDesc.shortcut,
+            desc: cmdDesc
+          });
+
+          if (cmdDesc.hotkey) {
+            hotkeys.add({
+              combo: cmdDesc.hotkey,
+              description: cmdDesc.name,
+              callback: e => {
+                e.preventDefault();
+                if (cmdSvc.canExec(cmdDesc)) {
+                  cmdSvc.exec(cmdDesc);
+                }
+              }
+            });
+          }
+        }]);
+      }
+
+      this._commands = _.sortBy(this._commands, 'name');
+
+      this._commandsByName = _.keyBy(this._commands, 'name');
+
+      hotkeys.add({
+        combo: 'ctrl+shift+p',
+        description: 'Show command palette',
+        callback: (e, c) => {
+          console.log(c);
+          e.preventDefault();
+          _this.show();
+        }
+      });
+    }
+
+    show() {
+      let commands = this._getCommands();
+      return this._ListSelectDialog.open(commands)
+        .result.then(cmd => {
+          return cmd.service.exec(cmd.desc);
+        });
+    }
+
+    exec(commandName) {
+      let command = this._getReqCommand(commandName);
+      if (this._canExec(command)) {
+        return this._$q.when(command.service.exec(command.desc));
+      } else {
+        return this._$q.when();
+      }
+    }
+
+    canExec(commandName) {
+      let command = this._getReqCommand(commandName);
+      return this._canExec(command);
+    }
+
+    getCommand(commandName) {
+      return this._commandsByName[commandName];
+    }
+
+    _canExec(command) {
+      return this._.isUndefined(command.service.canExec) || command.service.canExec(command.desc);
+    }
+
+    _getCommands() {
+      return this._commands.filter(cmd => this._.isUndefined(cmd.service.canExec) || cmd.service.canExec(cmd.desc));
+    }
+
+    _getReqCommand(commandName) {
+      let command = this.getCommand(commandName);
+      if (!command) {
+        throw new Error(`Missing command ${commandName}`);
+      }
+      return command;
+    }
+  }
+
+  angular.module('editorApp')
+    .service('CommandPalette', CommandPalette);
+})();
---
+++ editor/client/app/tree/commands/commandPaletteCfg.provider.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function() {
+
+  class CommandPaletteCfg {
+
+    constructor() {
+      this.commands = [];
+    }
+
+    /**
+     * register context menu action for services
+     * @param  {Object} commandDesc
+     * @param  {String} commandDesc.service - name of the service, that handles the specified command
+     * @param  {String} (optional) commandDesc.name - command title to be displayed
+     * @param  {String} (optional) commandDesc.icon - command icon
+     * @param  {String} (optional) commandDesc.shortcut - keyboard shortcut for the command
+     */
+    addCommand(commandDesc) {
+      this.commands.push(commandDesc);
+    }
+
+    $get() {
+      return this;
+    }
+  }
+
+  angular.module('editorApp')
+    .provider('CommandPaletteCfg', CommandPaletteCfg);
+})();
