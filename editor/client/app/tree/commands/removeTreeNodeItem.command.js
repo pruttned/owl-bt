5f27e439228ee5155aa6923d6dc7b2d6e51c7b12
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-06-18 21:46:03 +0200

    feat(client/removeTreeNodeItemCommand): make above item selected when removing an item

--- editor/client/app/tree/commands/removeTreeNodeItem.command.js
+++ editor/client/app/tree/commands/removeTreeNodeItem.command.js
@@ -4,13 +4,14 @@
 
   class RemoveTreeNodeItemCommand {
 
-    constructor(_, RemoveTreeNodeAction, RemoveTreeNodeSubItemAction, TreeNodeByIdStore, TreeStore, TreeSelection) {
+    constructor(_, RemoveTreeNodeAction, RemoveTreeNodeSubItemAction, TreeNodeByIdStore, TreeStore, TreeSelection, TreeNode) {
       this._ = _;
       this._RemoveTreeNodeAction = RemoveTreeNodeAction;
       this._RemoveTreeNodeSubItemAction = RemoveTreeNodeSubItemAction;
       this._TreeNodeByIdStore = TreeNodeByIdStore;
       this._TreeStore = TreeStore;
       this._TreeSelection = TreeSelection;
+      this._TreeNode = TreeNode;
     }
 
     canExec() {
@@ -20,14 +21,19 @@
     exec() {
       if (this.canExec()) {
         let selItem = this._TreeSelection.selItem();
-        if (this._TreeSelection.selItemType() === 'node') {
+        let selNode = this._TreeSelection.selNode();
+        let aboveItem = this._TreeNode.getAboveNodeItem(selNode, selItem);
+        let selItemType = this._TreeSelection.selItemType();
+
+        this._TreeSelection.select(aboveItem.node, aboveItem.item);
+
+        if (selItemType === 'node') {
           let parentNode = this._TreeNodeByIdStore.getNode(selItem.$meta.parentId);
           return this._RemoveTreeNodeAction.exec({
             node: parentNode,
             childNode: selItem,
           });
         } else {
-          let selNode = this._TreeSelection.selNode();
           return this._RemoveTreeNodeSubItemAction.exec({
             node: selNode,
             subItem: selItem
