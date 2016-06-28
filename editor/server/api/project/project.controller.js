3f2a6c58f99331d868d6157d727987f8f29b49f3
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2016-06-28 22:04:45 +0200

    refactor(server/project): extract reusable parts from project ctrl to separate module

--- editor/server/api/project/project.controller.js
+++ editor/server/api/project/project.controller.js
@@ -1,35 +1,7 @@
 'use strict';
 
-const bluebird = require('bluebird');
-const fs = bluebird.promisifyAll(require("fs"));
 const path = require('path');
-const errors = require('errno-codes');
-
-//TODO: extract
-const projectFileName = 'owl-bt.json';
-
-function getParentPath(absolutePath) {
-  let parentPath = path.resolve(absolutePath, '..');
-  if (parentPath === absolutePath) {
-    return null;
-  }
-  return parentPath;
-}
-
-function findProjectFile(currentAbsolutePath) {
-  let projectAbsolutePath = path.join(currentAbsolutePath, projectFileName);
-  return fs.readFileAsync(projectAbsolutePath, 'utf8')
-    .catch(err => {
-      if (err.code !== errors.ENOENT.code) {
-        throw err;
-      } else {
-        var parentPath = getParentPath(currentAbsolutePath);
-        if (parentPath) {
-          return findProjectFile(parentPath);
-        }
-      }
-    });
-}
+const project = require('../../components/project');
 
 exports.index = function(req, res, next) {
   let currentPath = req.query.path;
@@ -43,12 +15,12 @@ exports.index = function(req, res, next) {
   }
 
   let pathDir = path.dirname(currentPath);
-  findProjectFile(pathDir)
-    .then(prjContent => {
-      if (!prjContent) {
+  project.getProject(pathDir)
+    .then(prj => {
+      if (!prj) {
         res.status(404).send('No project found');
-      }else{
-        res.contentType('application/json').send(prjContent)
+      } else {
+        res.contentType('application/json').send(prj.content)
       }
     })
     .catch(err => {
---
+++ editor/server/components/project/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const path = require('path');
+const errors = require('errno-codes');
+const bluebird = require('bluebird');
+const fs = bluebird.promisifyAll(require("fs"));
+
+const projectFileName = 'owl-bt.json';
+
+
+function getParentPath(absolutePath) {
+  let parentPath = path.resolve(absolutePath, '..');
+  if (parentPath === absolutePath) {
+    return null;
+  }
+  return parentPath;
+}
+
+function getProject(currentAbsolutePath) {
+  let projectAbsolutePath = path.join(currentAbsolutePath, projectFileName);
+  return fs.readFileAsync(projectAbsolutePath, 'utf8')
+    .then(prjContent => ({
+      path : projectAbsolutePath,
+      content : prjContent
+    }))
+    .catch(err => {
+      if (err.code !== errors.ENOENT.code) {
+        throw err;
+      } else {
+        var parentPath = getParentPath(currentAbsolutePath);
+        if (parentPath) {
+          return getProject(parentPath);
+        }
+      }
+    });
+}
+
+module.exports = {
+  getProject: getProject
+};
