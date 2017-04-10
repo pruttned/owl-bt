8d753f801d6b936b22b3dec4eb74f0934602d3d5
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-10 20:34:15 +0200

    chore(gruntfile): fix module name in ngtemplates task

--- editor/Gruntfile.js
+++ editor/Gruntfile.js
@@ -323,7 +323,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: 'tmpApp',
+        module: 'editorApp',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
