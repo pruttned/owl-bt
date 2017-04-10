55426d4217855323bb9249184d85564cde94bbf6
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-10 13:53:59 +0200

    refactor(server/config): remove unused props

--- editor/server/config/environment/index.js
+++ editor/server/config/environment/index.js
@@ -23,18 +23,6 @@ var all = {
 
   // Server IP
   ip: process.env.IP || '0.0.0.0',
-
-  // Should we populate the DB with sample data?
-  seedDB: false,
-
-  // Secret for session, you will want to change this and make it an environment variable
-  secrets: {
-    session: 'editor-secret'
-  },
-
-  // List of user roles
-  userRoles: ['guest', 'user', 'admin'],
-
 };
 
 // Export the config object based on the NODE_ENV
