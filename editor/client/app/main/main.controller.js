b897bf5c75878d0a76ca9c26fca1d58bc7bd22c8
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-10 22:22:57 +0200

    refactor(main): remove tmp code

--- editor/client/app/main/main.controller.js
+++ editor/client/app/main/main.controller.js
@@ -2,24 +2,6 @@
 
 angular.module('editorApp')
   .controller('MainCtrl', function($scope, $http, $resource, ProjectStore) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.myTest = {};
-
-    $scope.callSrv = function() {
-      let MyTestResource = $resource('/api/myTest/:id');
-
-      $scope.myTest = MyTestResource.get({
-        id: 22
-      }, function(data) {
-        console.log(data);
-      });
-
-    };
 
     $scope.projects = ProjectStore.projects;
 
