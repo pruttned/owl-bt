d85d226b1412341931c92b7ec2f05393f048aab1
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-11 22:28:11 +0200

    feat(cmd): set default NODE_ENV to production

--- editor/bin/index.js
+++ editor/bin/index.js
@@ -1,8 +1,9 @@
+#!/usr/bin/env node
 'use strict';
 
 const yargs = require('yargs');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const argv = yargs
     .commandDir('cmds')
