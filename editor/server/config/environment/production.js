aa5f52b643f3c66861ad281b65bc71cfc78fba38
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-12 11:01:09 +0200

    feat(server/config): move production config to rc

--- editor/server/config/environment/production.js
+++ editor/server/config/environment/production.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const rc = require('rc');
+
+const rcCfg = rc('owlbt', {
+  port : 8955
+});
+
 // Production specific configuration
 // =================================
 module.exports = {
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            8080,
+            rcCfg.port,
 };
