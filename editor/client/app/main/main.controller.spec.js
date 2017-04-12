966800a4a596fdd32ee3df31fcb8dc0b0cfd8aca
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-12 18:20:49 +0200

    refactor(client/main): disable tmp test

--- editor/client/app/main/main.controller.spec.js
+++ editor/client/app/main/main.controller.spec.js
@@ -1,28 +1,28 @@
-'use strict';
+// 'use strict';
 
-describe('Controller: MainCtrl', function () {
+// describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('editorApp'));
+//   // load the controller's module
+//   beforeEach(module('editorApp'));
 
-  let MainCtrl,
-      scope,
-      $httpBackend;
+//   let MainCtrl,
+//       scope,
+//       $httpBackend;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+//   // Initialize the controller and a mock scope
+//   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+//     $httpBackend = _$httpBackend_;
+//     $httpBackend.expectGET('/api/things')
+//       .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+//     scope = $rootScope.$new();
+//     MainCtrl = $controller('MainCtrl', {
+//       $scope: scope
+//     });
+//   }));
 
-  it('should attach a list of things to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
-  });
-});
+//   it('should attach a list of things to the scope', function () {
+//     $httpBackend.flush();
+//     expect(scope.awesomeThings.length).toBe(4);
+//   });
+// });
