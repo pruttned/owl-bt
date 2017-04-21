e1346f68e14c9da239a34dcd1c74ffebb3d05b4e
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-21 10:01:49 +0200

    feat(client/tree): add save tree command

---
+++ src/client/app/tree/commands/saveTree.command.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function () {
+
+  class SaveTreeCommand {
+
+    constructor(SaveTreeAction, TreeStore) {
+      this._SaveTreeAction = SaveTreeAction;
+      this._TreeStore = TreeStore;
+    }
+
+    canExec() {
+      return this._TreeStore.isLoaded;
+    }
+
+    exec() {
+      if (this.canExec()) {
+        this._SaveTreeAction.exec();
+      }
+    }
+  }
+
+  angular.module('editorApp')
+    .service('SaveTreeCommand', SaveTreeCommand)
+    .config(function (CommandPaletteCfgProvider) {
+      CommandPaletteCfgProvider.addCommand({
+        service: 'SaveTreeCommand',
+        name: 'core:Save',
+        icon: 'save',
+        hotkey: 'mod+s'
+      });
+    });
+})();
