cbcb41191c57a0eb45087f19e8ad4f019bef96a6
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-23 22:38:28 +0200

    fix(client/main): prevent tree open for empty path

--- src/client/app/main/main.controller.js
+++ src/client/app/main/main.controller.js
@@ -3,8 +3,10 @@
 angular.module('editorApp')
   .controller('MainCtrl', function ($location, $httpParamSerializer) {
     this.openTree = function (treePath) {
-      $location.path('/tree').search({
-        path: treePath
-      });
+      if (treePath) {
+        $location.path('/tree').search({
+          path: treePath
+        });
+      }
     }
   });
