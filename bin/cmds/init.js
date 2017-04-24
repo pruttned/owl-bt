c9794ebcb9a501fd09fb004d633c6fefce6a3b0f
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-24 17:38:11 +0200

    feat(cli): add init cmd

--- bin/cmds/init.js
+++ bin/cmds/init.js
@@ -1,9 +1,20 @@
+'use strict';
+
+const path = require('path'),
+    bluebird = require('bluebird'),
+    fs = bluebird.promisifyAll(require("fs"));
+
+
 function handler(argv) {
-    console.log('TODO: init');
+    const projDirPath = path.resolve(argv.path);
+    fs.readFileAsync(path.join(__dirname, '../templates/owl-bt.json'), 'utf8')
+        .then(template => fs.writeFileAsync(path.join(projDirPath, 'owl-bt.json'), template, {
+            flag: 'wx'
+        }));
 }
 
 module.exports = {
-    command: 'init',
-    describe: 'init new owl-bt project',
+    command: ['init [path]', 'i'],
+    describe: 'init new owl-bt project in a given directory',
     handler: handler
 }
