7af8377da83f858ac1f8de2e488b52a4c9a16933
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-24 17:42:02 +0200

    fix(cli/init): enable current dir

--- bin/cmds/init.js
+++ bin/cmds/init.js
@@ -6,7 +6,7 @@ const path = require('path'),
 
 
 function handler(argv) {
-    const projDirPath = path.resolve(argv.path);
+    const projDirPath = path.resolve(argv.path || '.');
     fs.readFileAsync(path.join(__dirname, '../templates/owl-bt.json'), 'utf8')
         .then(template => fs.writeFileAsync(path.join(projDirPath, 'owl-bt.json'), template, {
             flag: 'wx'
