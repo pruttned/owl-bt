a3895e21532035f8f510245eee2f67215eff534e
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-04-24 14:26:24 +0200

    feat(cli): add default run cmd

--- bin/cmds/run.js
+++ bin/cmds/run.js
@@ -4,18 +4,15 @@ const path = require('path'),
     serverConfig = require('../../src/server/config/environment');
 
 function handler(argv) {
-
-    const treePath = path.resolve(argv.path);
-
     server.run({
         success: () => {
-            opn(`${serverConfig.baseUrl}:${serverConfig.port}/tree?path=${encodeURIComponent(treePath)}`)
+            opn(`${serverConfig.baseUrl}:${serverConfig.port}`)
         }
     });
 }
 
 module.exports = {
-    command: ['open <path>', 'o'],
-    describe: 'open a tree file',
+    command: ['*'],
+    describe: 'run owlbt',
     handler: handler
 }
