a7e1f42296edd75a590b3227c0258d88c60ccf24
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-02-22 21:59:09 +0100

    chore: remove node-inspector

--- Gruntfile.js
+++ Gruntfile.js
@@ -196,15 +196,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Debugging with node inspector
-    'node-inspector': {
-      custom: {
-        options: {
-          'web-host': 'localhost'
-        }
-      }
-    },
-
     // Use nodemon to run server in debug mode with an initial breakpoint
     nodemon: {
       debug: {
@@ -411,8 +402,7 @@ module.exports = function (grunt) {
       ],
       debug: {
         tasks: [
-          'nodemon',
-          'node-inspector'
+          'nodemon'
         ],
         options: {
           logConcurrentOutput: true
