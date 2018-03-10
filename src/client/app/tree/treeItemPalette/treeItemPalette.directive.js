9c19e891288ae1687b1006d0bef341569ed0c3d0
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-03-11 00:03:20 +0100

    feat(treeItemPalette): divide items to groups

--- src/client/app/tree/treeItemPalette/treeItemPalette.directive.js
+++ src/client/app/tree/treeItemPalette/treeItemPalette.directive.js
@@ -2,86 +2,7 @@
   'use strict';
 
   angular.module('editorApp')
-    .directive('treeItemPalette', function (_, $compile, PropertyViewModelProvider, TreeSelection, ProjectStore,
-      TreeNodeProvider, TreeDecoratorItemProvider, TreeServiceItemProvider,
-      AddTreeNodeItemAction) {
-
-      let items = {};
-      const itemProviders = {
-        node: TreeNodeProvider,
-        decorator: TreeDecoratorItemProvider,
-        service: TreeServiceItemProvider,
-      }
-
-      function addItemIcon(liElm, item) {
-        if (item.icon) {
-          let iconElm = angular.element('<span class="fa fa-fw tree-item-palette-item_icon"></span>');
-          iconElm.addClass('fa-' + item.icon);
-          liElm.append(iconElm);
-        }
-      }
-      function setItemIcon(liElm, item) {
-        liElm.css('border-left-color', item.color);
-      }
-      
-      function renderItems(scope, items, ulElm) {
-        ulElm.empty();
-        items.forEach(function (item, index) {
-          let liElm = angular.element('<li class="tree-item-palette-item"/>');
-          liElm.on('click', () => {
-            let itemDesc = item.desc;
-            console.log(itemDesc);
-            let selNode = TreeSelection.selNode();
-
-            let nodeItem = itemProviders[item.itemType].create({
-              type: itemDesc.name
-            });
-            return AddTreeNodeItemAction.exec({
-              node: selNode,
-              item: nodeItem,
-              itemType: item.itemType
-            });
-          });
-
-          addItemIcon(liElm, item);
-          liElm.append(document.createTextNode(item.name));
-          setItemIcon(liElm, item);
-
-          //addItemAsideInf(liElm, item);
-          ulElm.append(liElm);
-        });
-      }
-
-      function renderAllItems(scope, element) {
-        return ProjectStore.ensureLoad().then(() => {
-          items.services = _.values(ProjectStore.getServiceDescs()).map(d => ({
-            name: d.name,
-            icon: d.icon,
-            desc: d,
-            itemType: 'service',
-            color: d.color || '#5cc690'
-          }));
-          items.decorators = _.values(ProjectStore.getDecoratorDescs()).map(d => ({
-            name: d.name,
-            icon: d.icon,
-            desc: d,
-            itemType: 'decorator',
-            color: d.color || '#5c90c6'
-          }));
-          items.nodes = _.values(ProjectStore.getNodeDescs()).map(d => ({
-            name: d.name,
-            icon: d.icon,
-            desc: d,
-            itemType: 'node',
-            color: d.color || (d.isComposite ? '#d08038' : '#955cc6')
-          }));
-
-          renderItems(scope, items.nodes, element.find('[data-nodes]>ul'))
-          renderItems(scope, items.decorators, element.find('[data-decorators]>ul'))
-          renderItems(scope, items.services, element.find('[data-services]>ul'))
-        });
-      }
-
+    .directive('treeItemPalette', function (TreeSelection) {
       return {
         templateUrl: 'app/tree/treeItemPalette/treeItemPalette.html',
         restrict: 'E',
@@ -89,19 +10,18 @@
         scope: {},
 
         link: function (scope, element) {
-
-          scope.$watch(() => ProjectStore.version, function () {
-            renderAllItems(scope, element)
-              .then(() => {
-
-              });
-          });
           scope.$watch(() => TreeSelection.selNode(), function () {
-            // updateItems(scope, element);
             let selNode = TreeSelection.selNode();
             scope.hasSelectedNode = selNode;
             scope.selNodeIsComposite = selNode && selNode.$meta.desc.isComposite;
           });
+
+          scope.compositeNodes = function (items) {
+            return items.filter(item => item.isComposite);
+          }
+          scope.actionNodes = function (items) {
+            return items.filter(item => !item.isComposite);
+          }
         }
       };
     });
