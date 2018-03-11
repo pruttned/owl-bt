c59dce7726d3a5dae85c5d13ac5f210bbb105c82
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-03-11 16:38:29 +0100

    feat(sidePanel): add expand/collapse

--- src/client/components/sidePanel/sidePanel.directive.js
+++ src/client/components/sidePanel/sidePanel.directive.js
@@ -1,15 +1,29 @@
 (function () {
   'use strict';
   angular.module('editorApp')
-    .directive('sidePanel', function () {
+    .directive('sidePanel', function (_) {
       return {
         templateUrl: 'components/sidePanel/sidePanel.html',
         restrict: 'E',
         replace: true,
         transclude: true,
         scope: {
-          title: '@'
+          title: '@',
+          name: '@'
         },
+        link: (scope) => {
+          const expandStorageKey = `panel-expand-${scope.name}`;
+          scope.isExpanded = localStorage.getItem(expandStorageKey);
+          if (_.isUndefined(scope.isExpanded)) {
+            scope.isExpanded = true;
+          } else {
+            scope.isExpanded = scope.isExpanded === 'true';
+          }
+          scope.expand = expand => {
+            scope.isExpanded = expand;
+            localStorage.setItem(expandStorageKey, scope.isExpanded);
+          }
+        }
       };
     });
 })();
