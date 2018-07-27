7124041f91a7d96932d2c2ba675556f44a1ebb37
daury guzman <me@dauryguzman.com>
2018-07-27 18:38:26 -0400

    pruttned/owl-bt#8 multiple inheritance unit test

--- src/client/app/tree/projectStore/projectStore.service.spec.js
+++ src/client/app/tree/projectStore/projectStore.service.spec.js
@@ -60,11 +60,63 @@ describe('Service: ProjectStore', () => {
             name: 'node3',
             icon: 'icon3'
           },
+          {
+            name: 'base-node2',
+            isAbstract: true,
+            isComposite: false,
+            icon: "car",
+            properties: [
+              {
+                "name": "baseProp1",
+                "default": "x",
+                "type": "string"
+              },
+              {
+                "name": "baseProp2",
+                "default": "y",
+                "type": "string"
+              }
+            ]
+          },
+          {
+            name: 'base-node3',
+            isAbstract: true,
+            icon: 'plane',
+            properties: [
+               {
+                 "name": "baseProp3",
+                 "default": "z",
+                 "type": "string"
+               },
+               {
+                 "name": "baseProp4",
+                 "default": "w",
+                 "type": "string"
+               }
+             ]
+
+          },
+          {
+            name: 'node4',
+            base: ['base-node2', 'base-node3'],
+            properties: [
+              {
+                "name": "baseProp3",
+                "default": "xy",
+                "type": "string"
+              },
+              {
+                "name": "baseProp4",
+                "default": "zw",
+                "type": "string"
+              }
+            ]
+          }
         ]
       };
       ProjectStore._compileProject(prj);
 
-      expect(Object.keys(ProjectStore.nodeTypeDescs).length).toBe(3);
+      expect(Object.keys(ProjectStore.nodeTypeDescs).length).toBe(4);
 
       const node1 = ProjectStore.nodeTypeDescs['node1'];
       expect(node1.name).toBe('node1');
@@ -79,7 +131,7 @@ describe('Service: ProjectStore', () => {
       expect(node1.properties[1].type).toBe('number');
       expect(node1.properties[2].name).toBe('TargetBlackboardKey');
       expect(node1.properties[2].default).toBe('yy');
-      expect(node1.properties[2].type).toBe('string');      
+      expect(node1.properties[2].type).toBe('string');
 
       const node2 = ProjectStore.nodeTypeDescs['node2'];
       expect(node2.name).toBe('node2');
@@ -98,6 +150,21 @@ describe('Service: ProjectStore', () => {
       expect(node3.icon).toBe('icon3');
       expect(node3.isComposite).toBeFalsy();
       expect(node3.properties).toBeUndefined();
+
+      const node4 = ProjectStore.nodeTypeDescs['node4'];
+      expect(node4.name).toBe('node4');
+      expect(node4.icon).toBe('plane');
+      expect(node4.isAbstract).toBeUndefined();
+      expect(node4.properties).toBeDefined();
+      expect(node4.properties.length).toBe(4);
+      expect(node4.properties[0].name).toBe('baseProp1');
+      expect(node4.properties[0].default).toBe('x');
+      expect(node4.properties[1].name).toBe('baseProp2');
+      expect(node4.properties[1].default).toBe('y');
+      expect(node4.properties[2].name).toBe('baseProp3');
+      expect(node4.properties[2].default).toBe('xy');
+      expect(node4.properties[3].name).toBe('baseProp4');
+      expect(node4.properties[3].default).toBe('zw');
     });
   });
 });
