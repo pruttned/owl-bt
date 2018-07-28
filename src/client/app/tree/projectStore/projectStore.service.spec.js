169a576cbe9e836a2a173ed82a276546d89e5671
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2018-07-28 18:55:45 +0200

    test(ProjectStore): extend inheritance tests

--- src/client/app/tree/projectStore/projectStore.service.spec.js
+++ src/client/app/tree/projectStore/projectStore.service.spec.js
@@ -64,17 +64,17 @@ describe('Service: ProjectStore', () => {
             name: 'base-node2',
             isAbstract: true,
             isComposite: false,
-            icon: "car",
+            icon: 'car',
             properties: [
               {
-                "name": "baseProp1",
-                "default": "x",
-                "type": "string"
+                'name': 'baseProp1',
+                'default': 'x',
+                'type': 'string'
               },
               {
-                "name": "baseProp2",
-                "default": "y",
-                "type": "string"
+                'name': 'baseProp2',
+                'default': 'y',
+                'type': 'string'
               }
             ]
           },
@@ -83,17 +83,22 @@ describe('Service: ProjectStore', () => {
             isAbstract: true,
             icon: 'plane',
             properties: [
-               {
-                 "name": "baseProp3",
-                 "default": "z",
-                 "type": "string"
-               },
-               {
-                 "name": "baseProp4",
-                 "default": "w",
-                 "type": "string"
-               }
-             ]
+              {
+                'name': 'baseProp2',
+                'default': 2,
+                'type': 'number'
+              },
+              {
+                'name': 'baseProp3',
+                'default': 'z',
+                'type': 'string'
+              },
+              {
+                'name': 'baseProp4',
+                'default': 'w',
+                'type': 'string'
+              }
+            ]
 
           },
           {
@@ -101,14 +106,13 @@ describe('Service: ProjectStore', () => {
             base: ['base-node2', 'base-node3'],
             properties: [
               {
-                "name": "baseProp3",
-                "default": "xy",
-                "type": "string"
+                'name': 'baseProp3',
+                'default': 'xy',
               },
               {
-                "name": "baseProp4",
-                "default": "zw",
-                "type": "string"
+                'name': 'baseProp4',
+                'default': 'zw',
+                'type': 'string'
               }
             ]
           }
@@ -122,28 +126,39 @@ describe('Service: ProjectStore', () => {
       expect(node1.name).toBe('node1');
       expect(node1.icon).toBe('icon1');
       expect(node1.isComposite).toBe(true);
-      expect(node1.properties.length).toBe(3);
-      expect(node1.properties[0].name).toBe('Prop1');
-      expect(node1.properties[0].default).toBe('xx');
-      expect(node1.properties[0].type).toBe('string');
-      expect(node1.properties[1].name).toBe('Speed');
-      expect(node1.properties[1].default).toBe(-1);
-      expect(node1.properties[1].type).toBe('number');
-      expect(node1.properties[2].name).toBe('TargetBlackboardKey');
-      expect(node1.properties[2].default).toBe('yy');
-      expect(node1.properties[2].type).toBe('string');
+      expect(node1.properties).toEqual([
+        {
+          'name': 'Prop1',
+          'default': 'xx',
+          'type': 'string'
+        },
+        {
+          'name': 'Speed',
+          'default': -1,
+          'type': 'number'
+        },
+        {
+          'name': 'TargetBlackboardKey',
+          'default': 'yy',
+          'type': 'string'
+        },
+      ]);
 
       const node2 = ProjectStore.nodeTypeDescs['node2'];
       expect(node2.name).toBe('node2');
-      expect(node2.icon).toBe('icon2');
-      expect(node2.isComposite).toBe(true);
-      expect(node2.properties.length).toBe(2);
-      expect(node2.properties[0].name).toBe('Prop1');
-      expect(node2.properties[0].default).toBe('xx');
-      expect(node2.properties[0].type).toBe('string');
-      expect(node2.properties[1].name).toBe('Speed');
-      expect(node2.properties[1].default).toBe(22);
-      expect(node2.properties[1].type).toBe('number');
+      expect(node2.icon).toBe('icon2')
+      expect(node2.properties).toEqual([
+        {
+          'name': 'Prop1',
+          'default': 'xx',
+          'type': 'string'
+        },
+        {
+          'name': 'Speed',
+          'default': 22,
+          'type': 'number'
+        }
+      ]);
 
       const node3 = ProjectStore.nodeTypeDescs['node3'];
       expect(node3.name).toBe('node3');
@@ -155,16 +170,28 @@ describe('Service: ProjectStore', () => {
       expect(node4.name).toBe('node4');
       expect(node4.icon).toBe('plane');
       expect(node4.isAbstract).toBeUndefined();
-      expect(node4.properties).toBeDefined();
-      expect(node4.properties.length).toBe(4);
-      expect(node4.properties[0].name).toBe('baseProp1');
-      expect(node4.properties[0].default).toBe('x');
-      expect(node4.properties[1].name).toBe('baseProp2');
-      expect(node4.properties[1].default).toBe('y');
-      expect(node4.properties[2].name).toBe('baseProp3');
-      expect(node4.properties[2].default).toBe('xy');
-      expect(node4.properties[3].name).toBe('baseProp4');
-      expect(node4.properties[3].default).toBe('zw');
+      expect(node4.properties).toEqual([
+        {
+          'name': 'baseProp1',
+          'default': 'x',
+          'type': 'string'
+        },
+        {
+          'name': 'baseProp2',
+          'default': 2,
+          'type': 'number'
+        },
+        {
+          'name': 'baseProp3',
+          'default': 'xy',
+          'type': 'string'
+        },
+        {
+          'name': 'baseProp4',
+          'default': 'zw',
+          'type': 'string'
+        }
+      ]);
     });
   });
 });
