45dd6cef92a7216bed0689314523926f21cf7cbf
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-06-06 12:25:51 +0200

    fix(formGroupParamEditor): remove self closing tags

--- src/client/app/tree/propertyEditor/formGroupParamEditor.directive.js
+++ src/client/app/tree/propertyEditor/formGroupParamEditor.directive.js
@@ -3,7 +3,7 @@
 angular.module('editorApp')
   .directive('formGroupParamEditor', function() {
     return {
-      template: '<div class="form-group"><label>{{property.name()}}</label><property-reset property="property"/><ng-transclude/></div>',
+      template: '<div class="form-group"><label>{{property.name()}}</label><property-reset property="property"></property-reset><ng-transclude></ng-transclude></div>',
       restrict: 'EA',
       replace: true,
       transclude: true,
