31b9363b491f7d9a8e1bc1d78b71ee7b56f9e334
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-06-06 11:53:31 +0200

    refactor(express.js): remove unused paths

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -35,31 +35,10 @@ module.exports = function(app) {
   }
 
   if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
-    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'dist')));
+    app.set('appPath', path.join(config.root, 'dist'));
+
     app.use(express.static(path.join(config.root, 'src/client')));
-    app.use('/babel-polyfill', express.static(path.join(config.root, 'node_modules/babel-polyfill/dist')));
-    app.use('/string_score', express.static(path.join(config.root, 'node_modules/string_score')));
-    app.use('/common', express.static(path.join(config.root, 'src/common')));
-    app.use('/bootstrap-sass', express.static(path.join(config.root, 'node_modules/bootstrap-sass')));
-    app.use('/font-awesome', express.static(path.join(config.root, 'node_modules/font-awesome')));
-    app.use('/jquery', express.static(path.join(config.root, 'node_modules/jquery/dist')));
-    app.use('/angular', express.static(path.join(config.root, 'node_modules/angular')));
-    app.use('/angular-bootstrap', express.static(path.join(config.root, 'node_modules/angular-bootstrap')));
-    app.use('/angular-cookies', express.static(path.join(config.root, 'node_modules/angular-cookies')));
-    app.use('/angular-resource', express.static(path.join(config.root, 'node_modules/angular-resource')));
-    app.use('/angular-sanitize', express.static(path.join(config.root, 'node_modules/angular-sanitize')));
-    app.use('/angular-route', express.static(path.join(config.root, 'node_modules/angular-route')));
-    app.use('/angular-hotkeys', express.static(path.join(config.root, 'node_modules/angular-hotkeys')));
-    app.use('/angular-ui-bootstrap', express.static(path.join(config.root, 'node_modules/angular-ui-bootstrap/dist')));
-    app.use('/d3', express.static(path.join(config.root, 'node_modules/d3')));
-    app.use('/undo-manager', express.static(path.join(config.root, 'node_modules/undo-manager')));
-    app.use('/lodash', express.static(path.join(config.root, 'node_modules/lodash')));
-    app.use('/toposort', express.static(path.join(config.root, 'node_modules/toposort-class/build')));
-    app.use('/jquery-mousewheel', express.static(path.join(config.root, 'node_modules/jquery-mousewheel')));
-    app.use('/malihu-custom-scrollbar-plugin', express.static(path.join(config.root, 'node_modules/malihu-custom-scrollbar-plugin')));
-    app.use('/ng-scrollbars', express.static(path.join(config.root, 'node_modules/ng-scrollbars/dist')));    
-    app.set('appPath', path.join(config.root, 'src/client'));
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
