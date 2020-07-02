ef095f86690166946ec33efaaa6369e36dd66817
Peter Ruttkay-Nedecky <ruttkayn@gmail.com>
2020-07-02 21:47:21 +0200

    test(setup): setup service mocks

--- jest/setup.js
+++ jest/setup.js
@@ -3,3 +3,6 @@ Object.defineProperty(window, 'jQuery', { value: jQuery });
 Object.defineProperty(window, '$', { value: jQuery });
 require('../src/client/index');
 require('angular-mocks');
+
+require('../__mocks__/projectStore.service.mock.js');
+require('../__mocks__/treeStore.service.mock.js');
